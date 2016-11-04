178466c4a93f563ef752096640b4b6eb15658052
Jared Stratton <strattonshrugged@gmail.com>
2016-11-03 22:24:11 -0700

    nav bars in place but not connected, movieInfo sending metadata to page

--- readme.md
+++ readme.md
@@ -1,6 +1,4 @@
 
-Arrow keys ...
-    rel="lightbox[groupName]" may be why arrows aren't displaying, no other images in same groupName so no arrows displayed
 
 Thumbnail ...
     popping. on page load first thumbnail appears on bottom of seekbar. why?!
@@ -12,15 +10,21 @@ Preview ..
 General ...
     Maybe handle undefined errors like going outside seekbar area
     Add movie meta data underneath the seek bar (title, producer, number of image currently selected (1 of 1000), etc
-    solve world hunger; tell no one
-    find Josh's note on solving world hunger, actually do it, don't even tell him
 
 code ...
     data bind hq-image title to name of image
 
-
-lightbox ...
-    hq image should be able to right click and open in new tab, easier to grab
-
 watermark ...
     figure out how to embed watermark, or at least experiment with it
+
+nav arrows ...
+    flickering during hovering, feels unstable
+    nav divs are hacked into place using styling, must be a better solution for placement, perhaps implement skeleton to see if it's any easier given additional features?
+    hook up left and right nav buttons to clicking the left and right buttons ... or ...
+    create functions for left and right movement; clicking on nav arrow or using left/right key fires them directly, both controlled by global timestamp quality, may be easier/slicker to implement than getting a click on a div to pretend it's a button push
+
+questions ...
+    create functions for left and right movement through array of preview images, ones that can control whether the nav arrows appear (so no left option on load, no right option on last image)
+    left arrow button and left nav arrow click call the left function, right arrow button and right nav arrow click call the right function, after either function timestamp is reset and position in array is tested to see if left/right nav arrows are displayed
+    would skeleton look cleaner at this point given the additional controls?
+    
--- static/js/movie.js
+++ static/js/movie.js
@@ -4,11 +4,13 @@ var ViewModel = function() {
     this.previewIndex = ko.observable(0);
     this.thumbnailIndex = ko.observable(0);
     this.seekPosition = ko.observable();
+    this.navVisibility = ko.observable(false);
     this.thumbnailVisibility = ko.observable(false);
     this.thumbnailOffsetX = ko.observable();
     this.thumbnailOffsetY = ko.observable();
     this.tracerWidth = ko.observable();
     this.tracerVisible = ko.observable(false);
+    this.textGoesHere = movieInfo.title;
     // this.previewUrl = ko.observable('http://cdn.wallpapersafari.com/13/80/3fOp6W.jpg');
     // set image source to empty string, see if that blows away the image
     this.hqUrl = ko.computed(function()     {
@@ -27,7 +29,24 @@ var ViewModel = function() {
         return percent * $('.seek-bar').width();
     }, this);
 
-    let timestamp = 0;  // aaaaaah, global variable ...
+    this.onSeek = function ()   {
+        this.thumbnailVisibility(true);
+        this.tracerVisible(true);
+    }
+
+    this.offSeek = function ()  {
+        this.thumbnailVisibility(false);
+        this.tracerVisible(false);
+    }
+
+    this.onNav = function   ()  {
+        this.navVisibility(true);
+    }
+    this.offNav = function   ()  {
+        this.navVisibility(false);
+    }
+
+    let timestamp = 0;  // eek, global variable ...
     this.onKeyDown = function (vm,downEvent)    {
         if (Date.now() - timestamp > 1000)  {
             if (downEvent.originalEvent.code == "ArrowRight")   {
@@ -42,17 +61,7 @@ var ViewModel = function() {
             }
             timestamp = Date.now();
         }
-        return true;
-    }
-
-    this.onSeek = function ()   {
-        this.thumbnailVisibility(true);
-        this.tracerVisible(true);
-    }
-
-    this.offSeek = function ()  {
-        this.thumbnailVisibility(false);
-        this.tracerVisible(false);
+        return true; // necessary to keep onKeyDown from blocking key commands from bubbling up to browser
     }
 
     this.onClickSeekbar = function (vm,clickEvent)  {
